fceba6470632d8148be3c7bfa028945b93596e10
Anthony Dest <37128846+AnthonyDest@users.noreply.github.com>
2023-12-22 02:42:43 -0500

    modifying simulating click

--- popup.js
+++ popup.js
@@ -29,9 +29,74 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-function listLiElementsFunction() {
-  var liElements = document.querySelectorAll("#D2L_LE_Content_TreeBrowser li");
-  liElements.forEach((li, index) => {
-    console.log(`List Element ${index + 1}:`, li);
+
+function simulateClick(element) {
+  var clickEvent = new Event('click', {
+    bubbles: true,
+    cancelable: true,
+    composed: true
   });
+  element.dispatchEvent(clickEvent);
 }
+
+function list_all_module_content() {
+
+  var list_elements = document.querySelectorAll('.d2l-datalist .d2l-link')
+
+
+  if (list_elements.length > 0) {
+    // Iterate over each li element
+    list_elements.forEach((element, index) => {
+      // Perform actions on each li element
+      console.log(`List Element_B ${index + 1}:`, element);
+      // element.click();
+      simulateClick(element);
+
+      download_to_pdf()
+
+      // needs a wait here to ensure previous stuff has time to load
+
+    });
+  } else {
+    console.log("No elements found.");
+  }
+}
+
+
+// content.js
+function listLiElementsFunction() {
+  // Select all <li> elements under #D2L_LE_Content_TreeBrowser
+  // aka outer list TOC navigation elements
+  var liElements = document.querySelectorAll("#D2L_LE_Content_TreeBrowser li .d2l-le-TreeAccordionItem-anchor");
+
+  // Check if there are any li elements
+  if (liElements.length > 0) {
+    // Iterate over each li element
+    liElements.forEach((liElement, index) => {
+      // Perform actions on each li element
+      console.log(`List Element_A ${index + 1}:`, liElement);
+      // liElement.click();
+      simulateClick(liElement);
+
+      list_all_module_content();
+
+      if (index === 2) {
+        return; // Stop the program execution
+      }
+
+      // needs a wait here to ensure previous stuff has time to load
+    });
+  } else {
+    console.log("No li elements found.");
+  }
+}
+
+
+
+
+function download_to_pdf() {
+
+  document.querySelectorAll(".d2l-page-main-padding button")[0].click()
+
+}
+
